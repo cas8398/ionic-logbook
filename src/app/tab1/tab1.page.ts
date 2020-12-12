a8032d3f50270664540809e3e20202c955d3cd45
cas8398 <cas8398@gmail.com>
2020-12-12 15:02:13 +0700

    Update tab1.page.ts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import {WordpressService} from '../services/wordpress.service';
 
 @Component({
@@ -13,32 +16,41 @@ export class Tab1Page implements OnInit {
   attachs: any[];
   thumbs =  new Map();
   thumbsArr: any[];
+  categoryName: string;
   page: number;
-  loaded:boolean;
-  loading:boolean;
-  constructor(public navCtrl: NavController,public wordpressService: WordpressService) {
+  loaded: boolean;
+  loading: boolean;
+
+  constructor(public navCtrl: NavController,public wordpressService: WordpressService, private route: ActivatedRoute, private router: Router, public toastCtrl: ToastController) {
   }
 
   getKeys(map){
     return Array.from(map.keys());
   }
 
+goTo(str: string){
+    this.router.navigate([ '/tabs/post/'+ str ]);
+  }
+
   loadPosts(){
-    this.loading = true;
+    this.loading = true; 
+
     if(this.wordpressService.wp_org){
       this.wordpressService.getPosts(this.page).subscribe(data => {
         this.items = data;
         for (let res of data) {
           if(!this.thumbs.has(res.id)){
-            this.thumbs.set(res.id, {id: res.id, title: res.title.rendered, content: res.content.rendered});
+            this.thumbs.set(res.id, {id: res.id, title: res.title.rendered, excerpt: res.excerpt.rendered});
           }        
         }
         this.loading = false;
+          document.getElementById("initoast").click();
         this.loaded = true;
-      });
-    }
+      }); 
+ }
     else {
-      this.wordpressService.getPosts(this.page).subscribe(data => {
+      this.wordpressService.getPosts(
+      this.page).subscribe(data => {
         this.items = data.posts;
         for (let res of data.posts) {
           if(!this.thumbs.has(res.ID)){
@@ -51,16 +63,23 @@ export class Tab1Page implements OnInit {
     }
   }
 
+  async openToast() {  
+ const toast = await this.toastCtrl.create({  
+      message: 'Berhasil memuat konten',  
+      duration: 2000  
+    });  
+    toast.present();  
+  }    
+  
+
   next() {
     this.page++;
-    this.loadPosts();
-  }
+    this.loadPosts(); 
+  } 
 
   ngOnInit() {
     this.loading = false;
     this.page = 1;
     this.loadPosts();
-  }
-
-}
-
+  }   
+ }
